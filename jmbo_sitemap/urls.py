c7f389452e1ef5d353ddd813527fd42cb8561a17
hedleyroos <hedleyroos@gmail.com>
2013-06-19 11:15:31 +0200

    Port XML sitemap over from jmbo-foundry

--- jmbo_sitemap/urls.py
+++ jmbo_sitemap/urls.py
@@ -2,9 +2,14 @@ from django.conf.urls.defaults import patterns, url
 
 from preferences import preferences
 
+from jmbo_sitemap import sitemaps
+
 
 urlpatterns = patterns(
     '',
+
+    (r'^sitemap\.xml$', 'jmbo_sitemap.sitemap', {'sitemaps': sitemaps}),
+
     url(
         r'^sitemap/$',
         'django.views.generic.simple.direct_to_template',
